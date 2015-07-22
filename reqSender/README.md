7aeca77fbbd6b973ada42c904a7db3d8ea0d2df8
mengjingleli <mengjinglei@qiniu.com>
2015-07-22 18:19:11 +0800

    change reqSender as a service

---
+++ reqSender/README.md
@@ -0,0 +1,17 @@
+# reqSender
+* reqSender是web界面的curl
+
+## 配置文件
+```
+{
+	"url"  : "http://127.0.0.1:7777",
+	"port" : 8800,
+	"cross_domain" : true,
+	"debug_level" : 0
+}
+
+* url: 需要请求的url
+* port: reqSender启动时绑定的端口
+* cross_domain: 是否启动cross domain
+
+```
---
+++ reqSender/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+
+	"github.com/qiniu/log.v1"
+	"qbox.us/cc/config"
+)
+
+type Proxy struct {
+	Url         string `json:"url"`
+	Port        int    `json:"port"`
+	CrossDomain bool   `json:"cross_domain"`
+	DebugLevel  int    `json:"debug_level"`
+}
+
+func (proxy *Proxy) sendHandler(w http.ResponseWriter, r *http.Request) {
+	index, _ := template.ParseFiles("index.html")
+	index.Execute(w, nil)
+}
+
+func (proxy *Proxy) staticHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+func (proxy *Proxy) handler(w http.ResponseWriter, r *http.Request) {
+
+	if proxy.CrossDomain {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization,Origin, X-Requested-With, Content-Type, Accept")
+	}
+
+	log.Info("recieve req url:", r.RequestURI)
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return
+	}
+	log.Info("req data:", string(data))
+
+	url := proxy.Url + r.RequestURI
+	req, err := http.NewRequest(r.Method, url, bytes.NewBuffer(data))
+	if err != nil {
+		log.Error(err)
+	}
+
+	copyHeader(req.Header, r.Header)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if resp != nil {
+		defer resp.Body.Close()
+	}
+	if err != nil {
+		return
+	}
+
+	ret, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+
+	fmt.Fprint(w, string(ret))
+
+}
+
+//copy header
+func copyHeader(dst, src http.Header) {
+
+	for k, vv := range src {
+		for _, v := range vv {
+			dst.Add(k, v)
+		}
+	}
+}
+
+func main() {
+
+	config.Init("f", "proxy", "proxy.conf")
+	var proxy Proxy
+	if err := config.Load(&proxy); err != nil {
+		log.Fatal("config.Load failed:", err)
+		return
+	}
+	log.SetOutputLevel(proxy.DebugLevel)
+
+	http.HandleFunc("/send/", proxy.sendHandler)
+	http.HandleFunc("/static/", proxy.staticHandler)
+	http.HandleFunc("/", proxy.handler)
+	err := http.ListenAndServe(":"+strconv.Itoa(proxy.Port), nil)
+	if err != nil {
+		log.Error(err)
+	}
+}
